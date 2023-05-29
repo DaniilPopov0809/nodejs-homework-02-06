9edfd198e891518b7ec5c754a8a2b3f10de9a208
Daniil Popov <x6uhrox@gmail.com>
2023-05-29 12:25:33 +0300

    FIX: version mongoose

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const app = require('./app')
+const app = require("./app");
+
+mongoose.set("strictQuery", true);
 
 const DB_HOST =
   "mongodb+srv://Daniil:PyySAGlF1PamnE7v@cluster0.ffokdus.mongodb.net/db_contacts?retryWrites=true&w=majority";
@@ -8,7 +10,6 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(3000);
-    console.log("Database connection successful")})
-  .catch((error) =>  console.log(error.message));
-
-
+    console.log("Database connection successful");
+  })
+  .catch((error) => console.log(error.message));
